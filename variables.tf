4f43afb4b5eedb6428a596c39973df53094e062d
barochiarg <barochiarg@gmail.com>
2026-01-07 17:57:33 +0530

    Add variables for VPC and public subnet IDs

--- variables.tf
+++ variables.tf
@@ -12,3 +12,13 @@ variable "instance_type" {
   description = "EC2 instance type"
   type        = string
 }
+
+variable "vpc_id" {
+  description = "Existing VPC ID"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "Existing public subnet ID"
+  type        = string
+}
